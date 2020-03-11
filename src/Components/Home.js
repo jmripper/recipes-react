8c7e84cd757e3ffc763b043bf5f0e03a1e182048
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-11 14:11:32 -0400

    added in background image to homepage component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Img from "../Images/spoons.jpg"
 
-const HomePage = () => {
+const ImageBackground = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${Img});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const HomePage = props => {
   return (
-    <>
-    </>
+      <ImageBackground src={Img} />
   );
-}
+};
 
-export default App;
+export default HomePage;
